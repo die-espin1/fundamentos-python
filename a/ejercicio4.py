71c85f340b8ddc23ee01bb78bae6d7c008215f1d
die-espin1 <120534064+die-espin1@users.noreply.github.com>
2025-05-31 15:38:02 -0600

    ejercicio4.py

--- a/ejercicio4.py
+++ a/ejercicio4.py
@@ -1,9 +1,1 @@
-n = int(input("Ingresa un número: "))
-
-if 1 <= n <= 10**18:
-    if n % 7 == 0 and n % 5 != 0:
-        print("Mágico")
-    else:
-        print("Normal")
-else:
-    print("Número fuera del rango permitido (1 ≤ n ≤ 10^18)")
+#ejercicio extra
